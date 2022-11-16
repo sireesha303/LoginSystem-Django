2e2961fb730e969713720392453af52d8860aded
sireesha <msireesha30031999@gmail.com>
2022-11-16 16:52:08 +0530

    added profile model and added same to admin site

--- LoginSystem_Django/settings.py
+++ LoginSystem_Django/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'userprofiles.apps.UserprofilesConfig',
+
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
