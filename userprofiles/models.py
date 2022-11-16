543ee84ad3be5f3bd37af4cc0a4489df34cdeffe
sireesha <msireesha30031999@gmail.com>
2022-11-16 16:43:12 +0530

    first commit

---
+++ userprofiles/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+
+class Profile(models.Model):
+    username = models.CharField(max_length=200, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    first_name = models.CharField(null=True, blank=True, max_length=100)
+    last_name = models.CharField(null=True, blank=True, max_length=100)
+    git_url = models.CharField(null=True, blank=True, max_length=100)
+    linkedin_url = models.CharField(null=True, blank=True, max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.username
